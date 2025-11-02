cca6758ceacb07626331fdc78b2e8db1563ecb12
VRATHIK SHENOY K <shenoyvrathik@gmail.com>
2025-11-02 11:04:47 +0530

    python_venv

---
+++ lua/plugins/venv.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim",
+      "mfussenegger/nvim-dap-python",
+    },
+    opts = {
+      -- Auto select venv when opening Python files
+      auto_refresh = true,
+    },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
+    },
+  },
+}
