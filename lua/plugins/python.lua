d8b0af669da26d5b2b4ca4ad92d50f413d2867da
VRATHIK SHENOY K <shenoyvrathik@gmail.com>
2026-01-16 11:31:15 +0530

    pythonlsp

---
+++ lua/plugins/python.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "openFilesOnly",
+              },
+            },
+          },
+        },
+      },
+    },
+    keys = {
+      { "<leader>lR", "<cmd>LspRestart<cr>", desc = "Restart LSP" },
+    },
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "mfussenegger/nvim-dap-python",
+    },
+    opts = {
+      auto_refresh = true,
+      search_venv_managers = true,
+      search_workspace = true,
+      dap_enabled = true,
+      hooks = {
+        PostrunActiveVenv = function()
+          vim.cmd("LspRestart")
+        end,
+      },
+    },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
+    },
+  },
+}
--- lua/plugins/venv.lua
+++ lua/plugins/venv.lua
@@ -56,19 +56,4 @@ return {
       require("dap-python").setup()
     end,
   },
-  {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "mfussenegger/nvim-dap-python",
-    },
-    opts = {
-      -- Auto select venv when opening Python files
-      auto_refresh = true,
-    },
-    event = "VeryLazy",
-    keys = {
-      { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" },
-    },
-  },
 }
